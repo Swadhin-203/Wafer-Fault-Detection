5d37384e5a6c3ed1fca2253594d24e3bb8336c88
Swadhin-203 <nayakswadhingyanajyoti@gmail.com>
2023-07-15 20:35:09 +0530

    first commit for my project

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Wafer Fault Detection",
+    version="0.0.1",
+    author="Swadhin Gyanajyoti Nayak",
+    author_email="nayakswadhingyanajyoti@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
+
